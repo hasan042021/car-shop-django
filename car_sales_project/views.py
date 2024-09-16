92697fdd98f17622f06230798c75e852cf99ceba
Hasan <hasan.dev04@gmail.com>
2024-09-16 11:43:35 +0600

    filter design added

--- car_sales_project/views.py
+++ car_sales_project/views.py
@@ -5,12 +5,13 @@ from cars_app.models import Car, Brand
 def home(request, brand_slug=None):
 
     data = Car.objects.all()
-
+    filter = None
     if brand_slug is not None:
         brand = Brand.objects.get(slug=brand_slug)
         data = Car.objects.filter(brand=brand)
+        filter = brand.brand_name
 
     brands = Brand.objects.all()
     return render(
-        request, "home.html", {"data": data, "brands": brands, "filter": brand_slug}
+        request, "home.html", {"data": data, "brands": brands, "filter": filter}
     )
